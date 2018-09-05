2626a6a9049b65ac6c22a241ba927f67495f391b
thiminhnhut <thiminhnhut@gmail.com>
2018-09-05 17:42:41 +0700

    Singleton Example

---
+++ SingletonExamples/app/src/main/java/io/github/thiminhnhut/singletonexamples/SingletonSession.kt
@@ -0,0 +1,25 @@
+package io.github.thiminhnhut.singletonexamples
+
+class SingletonSession private constructor(){
+    private lateinit var username: String
+
+    init {
+        println("Create Object with Singleton")
+    }
+
+    private object Holder {
+        val instance = SingletonSession()
+    }
+
+    companion object {
+        val instance: SingletonSession by lazy { Holder.instance }
+    }
+
+    fun getUsername(): String {
+        return username
+    }
+
+    fun setUsername(username: String) {
+        this.username = username
+    }
+}
