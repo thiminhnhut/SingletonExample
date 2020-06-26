0db5cdc16aaf1660fa6e2fb827f1df2a37364c74
thiminhnhut <thiminhnhut@gmail.com>
2020-06-26 09:51:24 +0700

    Restructure project

---
+++ app/src/main/java/io/github/thiminhnhut/singletonexamples/MainActivity.kt
@@ -0,0 +1,22 @@
+package io.github.thiminhnhut.singletonexamples
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            val username = edtUsername.text.toString()
+            SingletonSession.instance.setUsername(username)
+
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
